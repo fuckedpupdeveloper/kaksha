6a7c7c4da01d14dc24ff6de9ee5cc735f4d35c51
raju <33912085+fuckedpupdeveloper@users.noreply.github.com>
2018-03-16 23:37:57 +0530

    Update About1.java

--- app/src/main/java/com/kaksha/karanbagle/kaksha/About1.java
+++ app/src/main/java/com/kaksha/karanbagle/kaksha/About1.java
@@ -10,4 +10,23 @@ public class About1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about1);
     }
+    
+    @Override
+public void onBackPressed() {
+    new AlertDialog.Builder(this)
+        .setIcon(android.R.drawable.ic_dialog_alert)
+        .setTitle("Closing Activity")
+        .setMessage("Are you sure you want to close this activity?")
+        .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+    {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            finish();    
+        }
+
+    })
+    .setNegativeButton("No", null)
+    .show();
+}
+    
 }
